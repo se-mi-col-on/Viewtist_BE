f9cfc5ca04c3b233cb5783dd7df671f34c0f4369
limHyeonTaek <143856509+limHyeonTaek@users.noreply.github.com>
2024-02-26 10:07:22 +0900

    setting: 프로젝트 초기 설정

---
+++ src/main/java/semicolon/viewtist/ViewtistApplication.java
@@ -0,0 +1,13 @@
+package semicolon.viewtist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewtistApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ViewtistApplication.class, args);
+  }
+
+}
---
+++ src/test/java/semicolon/viewtist/ViewtistApplicationTests.java
@@ -0,0 +1,13 @@
+package semicolon.viewtist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewtistApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
