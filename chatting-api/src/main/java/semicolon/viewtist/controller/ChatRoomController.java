2b2dbb0efd89931b8377de3eaab28ad9cf0290a8
redandsilver <129943670+redandsilver@users.noreply.github.com>
2024-03-13 21:41:31 +0900

    Update ChatRoomController.java

--- chatting-api/src/main/java/semicolon/viewtist/controller/ChatRoomController.java
+++ chatting-api/src/main/java/semicolon/viewtist/controller/ChatRoomController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
