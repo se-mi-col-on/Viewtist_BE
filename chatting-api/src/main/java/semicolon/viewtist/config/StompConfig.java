fdef03e73a7de84c47d071681a4de5b794ea862a
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-03-13 13:56:22 +0900

    refactor: 안쓰는 import & 디렉토리 정리

--- chatting-api/src/main/java/semicolon/viewtist/config/StompConfig.java
+++ chatting-api/src/main/java/semicolon/viewtist/config/StompConfig.java
@@ -4,12 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import semicolon.viewtist.stomp.StompHandler;
 
