27619a0440473f2f37b8c325dea7698f17d25801
redandsilver <129943670+redandsilver@users.noreply.github.com>
2024-03-16 22:57:04 +0900

    Update StompHandler.java

--- chatting-api/src/main/java/semicolon/viewtist/stomp/StompHandler.java
+++ chatting-api/src/main/java/semicolon/viewtist/stomp/StompHandler.java
@@ -24,40 +24,8 @@ public class StompHandler implements ChannelInterceptor {
   @Override
   public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
     StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-    if (accessor.getCommand() == StompCommand.CONNECT) {
-      // todo check jwt
-      log.info("CONNECT ");
-    }else if(accessor.getCommand() == StompCommand.SUBSCRIBE){
-      log.info("SUBSCRIBE {} ", message.toString());
-      String streamKey = extractDestination(
-          (String) message.getHeaders().get("simpDestination"));
-
-      if (streamKey == null) {
-        throw new ChattingException(ErrorCode.NOT_EXIST_DESTINATION);
-      }
-      ChatRoom chatRoom = chatRoomRepository.findByStreamKey(streamKey).orElseThrow(
-          () ->  new ChattingException(ErrorCode.NOT_EXIST_STREAMKEY)
-      );
-      if(!chatRoom.isActive()){
-        throw new ChattingException(ErrorCode.DISABLED_CHAT_ROOM);
-      }
-      log.info(streamKey);
-
-    }else if (StompCommand.DISCONNECT == accessor.getCommand()) {
-//      String sessionId = (String) message.getHeaders().get("simpSessionId");
-//      log.info("DISCONNECT {}",sessionId);
-    }
+  
     return message;
   }
-  private static String extractDestination(String input) {
-    String pattern = "/(\\w+)$"; // 정규표현식 패턴
-    Pattern r = Pattern.compile(pattern);
-    Matcher m = r.matcher(input);
-
-    if (m.find()) {
-      return m.group(1); // 매칭된 부분 반환
-    } else {
-      return null; // 매칭된 부분이 없을 경우 null 반환 또는 적절한 오류 처리
-    }
-  }
+  
 }
