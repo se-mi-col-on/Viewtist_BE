9018512db8da0bedf6df33f7bebcca138130fdb5
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-03-17 14:14:05 +0900

    refactor: 채팅 연결 및 채팅 전송을 확인하기 위한 코드
    유저나 접근 권한 검증은 생략하고 배포시 연결되어 채팅 전송이 가능한지만 확인하기 위해 수정

--- chatting-api/src/main/java/semicolon/viewtist/stomp/StompHandler.java
+++ chatting-api/src/main/java/semicolon/viewtist/stomp/StompHandler.java
@@ -1,20 +1,13 @@
 package semicolon.viewtist.stomp;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
-import semicolon.viewtist.chatting.entity.ChatRoom;
 import semicolon.viewtist.chatting.repository.ChatRoomRepository;
-import semicolon.viewtist.exception.ChattingException;
-import semicolon.viewtist.global.exception.ErrorCode;
 
 @RequiredArgsConstructor
 @Component
