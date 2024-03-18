b31f4e0d4d08d6449a9bb52f358b9815c48d5964
limHyeonTaek <lht990e@naver.com>
2024-03-18 20:34:02 +0900

    fix: post

--- common/src/main/java/semicolon/viewtist/CommonApplication.java
+++ common/src/main/java/semicolon/viewtist/CommonApplication.java
@@ -16,9 +16,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients(basePackages = "semicolon.viewtist.mailgun")
 @EnableJpaRepositories(basePackages = {"semicolon.viewtist.user.repository",
-    "semicolon.viewtist.jwt.repository", "semicolon.viewtist.image.repository", "semicolon.viewtist.sse.repository"})
+    "semicolon.viewtist.jwt.repository", "semicolon.viewtist.image.repository",
+    "semicolon.viewtist.sse.repository", "semicolon.viewtist.post.repository"})
 @EntityScan(basePackages = {"semicolon.viewtist.user.entity", "semicolon.viewtist.jwt.entity",
-    "semicolon.viewtist.image.entity","semicolon.viewtist.sse.entity"})
+    "semicolon.viewtist.image.entity", "semicolon.viewtist.sse.entity",
+    "semicolon.viewtist.post.entity"})
 @SpringBootApplication
 @ImportAutoConfiguration({FeignAutoConfiguration.class, HttpClientConfiguration.class})
 @ServletComponentScan
