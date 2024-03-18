94d10ad0058ddf6409841ff2a1ac6f3dba096840
limHyeonTaek <lht990e@naver.com>
2024-03-18 19:45:01 +0900

    fix: post

--- common/src/main/java/semicolon/viewtist/post/service/PostService.java
+++ common/src/main/java/semicolon/viewtist/post/service/PostService.java
@@ -1,16 +1,18 @@
 package semicolon.viewtist.post.service;
 
-import jakarta.persistence.EntityNotFoundException;
+import static semicolon.viewtist.global.exception.ErrorCode.ACCESS_DENIED;
+
 import jakarta.transaction.Transactional;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import semicolon.viewtist.global.exception.ErrorCode;
 import semicolon.viewtist.post.dto.request.PostWriteRequest;
 import semicolon.viewtist.post.dto.request.UpdatePostRequest;
 import semicolon.viewtist.post.dto.response.PostResponse;
 import semicolon.viewtist.post.entity.Post;
+import semicolon.viewtist.post.exception.PostException;
 import semicolon.viewtist.post.repository.PostRepository;
 import semicolon.viewtist.user.entity.User;
 import semicolon.viewtist.user.repository.UserRepository;
@@ -19,8 +21,8 @@ import semicolon.viewtist.user.repository.UserRepository;
 @RequiredArgsConstructor
 public class PostService {
 
-    private PostRepository postRepository;
-    private UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public PostResponse postWrite(PostWriteRequest postWriteRequest) {
         // 게시물 작성 로직을 구현
@@ -37,11 +39,11 @@ public class PostService {
     public void updatePost(Long postId, UpdatePostRequest updatePostRequest,
             Authentication authentication) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new EntityNotFoundException("해당하는 글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new PostException(ErrorCode.POST_NOT_FOUND));
 
         // 사용자의 인증 정보를 이용하여 사용자를 식별
         User user = userRepository.findByEmail(authentication.getName())
-                .orElseThrow(() -> new EntityNotFoundException("해당하는 글을 찾을 수 없습니다."));
+                .orElseThrow(() -> new PostException(ErrorCode.POST_NOT_FOUND));
 
         // 현재 글의 작성자와 로그인한 사용자의 ID를 비교하여 같은 사용자인지 확인
         if (Objects.equals(post.getUser().getId(), user.getId())) {
@@ -49,7 +51,7 @@ public class PostService {
             post.setContent(updatePostRequest.getContent());
             postRepository.save(post);
         } else {
-            throw new AccessDeniedException("해당 글에 대한 권한이 없습니다.");
+            throw new PostException(ACCESS_DENIED);
         }
     }
 
