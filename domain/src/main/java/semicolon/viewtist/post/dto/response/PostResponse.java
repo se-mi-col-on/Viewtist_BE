c1c63d6569009f241e09b4d5d24fc843d683736e
limHyeonTaek <lht990e@naver.com>
2024-03-19 19:13:37 +0900

    refactor: response에 createdAt 추가

--- domain/src/main/java/semicolon/viewtist/post/dto/response/PostResponse.java
+++ domain/src/main/java/semicolon/viewtist/post/dto/response/PostResponse.java
@@ -1,5 +1,6 @@
 package semicolon.viewtist.post.dto.response;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,20 +11,23 @@ import semicolon.viewtist.post.entity.Post;
 @Builder
 public class PostResponse {
 
-    private Long id;
+  private Long id;
 
-    private String title;
+  private String title;
 
-    private String content;
+  private String content;
 
-    private String nickname;
+  private String nickname;
 
-    public static PostResponse from(Post post) {
-        return PostResponse.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .content(post.getContent())
-                .nickname(post.getUser().getNickname())
-                .build();
-    }
+  private LocalDateTime createdAt;
+
+  public static PostResponse from(Post post) {
+    return PostResponse.builder()
+        .id(post.getId())
+        .title(post.getTitle())
+        .content(post.getContent())
+        .nickname(post.getUser().getNickname())
+        .createdAt(post.getCreatedAt())
+        .build();
+  }
 }
