7f09e91756236c7f602eb28cd9b7a4c7821fdfe8
limHyeonTaek <lht990e@naver.com>
2024-03-25 11:18:05 +0900

    refactor: oauth 식별자를 email로 변경

--- common/src/main/java/semicolon/viewtist/oauth/handler/OAuth2SuccessHandler.java
+++ common/src/main/java/semicolon/viewtist/oauth/handler/OAuth2SuccessHandler.java
@@ -32,7 +32,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
       Authentication authentication) throws IOException, ServletException {
     CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
     String userId = oAuth2User.getName();
-    User user = userRepository.findByNickname(userId)
+    User user = userRepository.findByEmail(userId)
         .orElseThrow(() -> new UserException(USER_NOT_FOUND));
 
     String accessToken = tokenProvider.generateToken(user);
